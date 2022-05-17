34004e21e49cea5f41a572eddde78a46bcee3aa3
wick <wickham.eaw@gmail.com>
2022-05-17 13:30:20 -0400

    configuring global style guide

--- libs/style-guide/src/lib/style-guide.spec.js
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import StyleGuide from './style-guide';
-describe('StyleGuide', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<StyleGuide />);
-    expect(baseElement).toBeTruthy();
-  });
-});
