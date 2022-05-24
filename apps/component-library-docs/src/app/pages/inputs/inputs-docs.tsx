872d18a62d9114c303aa706371874c35b1def7f3
wick <wickham.eaw@gmail.com>
2022-05-24 11:49:54 -0400

    added input onchange method

--- apps/component-library-docs/src/app/pages/inputs/inputs-docs.tsx
+++ apps/component-library-docs/src/app/pages/inputs/inputs-docs.tsx
@@ -1,11 +1,28 @@
 import { Input } from '@cc/component-library';
+import { useState, ChangeEvent } from 'react';
 
 export default function InputsDocs() {
+  const [value, setValue] = useState('');
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
   return (
     <div>
       <h1>Inputs</h1>
       <p>The input component ...</p>
-      <Input />
+      <Input
+        id="test"
+        name="tester"
+        value={value}
+        type="text"
+        placeholder="test input"
+        disabled={false}
+        required={false}
+        readonly={false}
+        onChange={handleChange}
+      />
     </div>
   );
 }
--- libs/component-library/src/components/input/input.tsx
+++ libs/component-library/src/components/input/input.tsx
@@ -1,34 +1,38 @@
-import { FC } from 'react';
+import { FC, ChangeEvent } from 'react';
 import './input.scss';
 
 export interface ccInput {
-  accept?: null;
-  alt?: string;
-  autocomplete?: null;
-  checked?: boolean;
-  dirname?: string;
-  disabled?: boolean;
-  form?: boolean;
-  formaction?: null;
-  formenctype?: null;
-  formmethod?: null;
-  formonvalidate?: null;
-  formtarget?: null;
+  // accept?: null; // only valid for file input type, defines selectable file types
+  // alt?: string; // only valid for image input type, required for accessibility
+  // autocomplete?: null;
+  // checked?: boolean;
+  // dirname?: string;
+  disabled?: boolean; // makes the input disabled
+  // error?: boolean;
+  // form?: boolean;
+  // formaction?: null;
+  // formenctype?: null;
+  // formmethod?: null;
+  // formonvalidate?: null;
+  // formtarget?: null;
+  fullWidth?: boolean; // makes input the full length of parent container
+  // helperText?: string; //text used to describe error to better assist user
   id?: string;
-  label?: string;
-  max?: number;
-  maxlength?: number;
-  min?: number;
-  minlength?: number;
-  multiple?: boolean;
-  name?: string;
-  pattern?: string;
-  placeholder?: string;
-  readonly?: boolean;
-  required?: boolean;
-  size?: null;
-  src?: null;
-  step?: null;
+  label?: string; // label for the input, necessary for accessibility
+  // max?: number;
+  maxlength?: number; // maximum length of input value
+  // min?: number;
+  minlength?: number; // minimum length of input value
+  // multiple?: boolean; // (email or file) means user can enter multiple files or emails
+  name?: string; // passed alongside value for form submission
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void; //  handles value changes
+  // pattern?: string; // regex that the value must pass to be submitted
+  placeholder?: string; // provides additional context about input
+  readonly?: boolean; // cannot edit value
+  required?: boolean; // user must input value
+  size?: 'small' | 'normal' | 'large'; // input size
+  // src?: null; // only valid for image input type, URL
+  // step?: null; // validation for numeric input types
   type?: 'text' | 'email' | 'number';
   value?: any;
 }
@@ -39,20 +43,17 @@ export const Input: FC<ccInput> = props => {
       {props.label}
       <input
         id={props.id}
-        alt={props.alt}
         className="input"
         name={props.name}
         value={props.value}
         type={props.type}
         maxLength={props.maxlength}
         minLength={props.minlength}
-        checked={props.checked}
         placeholder={props.placeholder}
         required={props.required}
-        pattern={props.pattern}
         readOnly={props.readonly}
         disabled={props.disabled}
-        dir-name={props.dirname}
+        onChange={props.onChange}
       />
     </label>
   );
