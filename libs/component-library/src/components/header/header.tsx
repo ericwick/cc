0518fba8790b61d20ae9aff06dce9b7e2657763a
wick <wickham.eaw@gmail.com>
2022-05-26 14:04:40 -0400

    header background blur

--- libs/component-library/src/components/header/header.tsx
+++ libs/component-library/src/components/header/header.tsx
@@ -1,14 +1,12 @@
 import './header.scss';
 
 /* eslint-disable-next-line */
-export interface HeaderProps {}
+export interface HeaderProps {
+  content: any;
+}
 
 export function Header(props: HeaderProps) {
-  return (
-    <div className="header">
-      <h1>Welcome to Header!</h1>
-    </div>
-  );
+  return <header className="header">{props.content}</header>;
 }
 
 export default Header;
