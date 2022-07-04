b6f7a98b252fff49f67bb4ac8623ce9932e31397
Eric Wickham <wickera74@gmail.com>
2022-07-04 17:58:24 -0400

    button skeleton

--- apps/component-library-docs/src/app/pages/button/button-docs.tsx
+++ apps/component-library-docs/src/app/pages/button/button-docs.tsx
@@ -7,7 +7,17 @@ export default function ButtonDocs() {
       <p>
         Buttons allow users to take actions, and make choices with a single tap.
       </p>
-      <Button />
+      <h1>Base Style</h1>
+      <Button onClick={() => null} content="base button" />
+      <Button onClick={() => null} content="disabled" disabled={true} />
+      <Button
+        onClick={() => null}
+        content="full width button"
+        fullWidth={true}
+      />
+      <Button onClick={() => null} content="small" size="small" />
+      <Button onClick={() => null} content="large" size="large" />
+      <h1>Variants</h1>
     </div>
   );
 }
--- libs/component-library/src/components/button/button.tsx
+++ libs/component-library/src/components/button/button.tsx
@@ -1,14 +1,45 @@
-import styles from './button.module.scss';
+import { FC, MouseEvent, FocusEventHandler } from 'react';
+import classname from 'classnames';
+import './button.scss';
 
 /* eslint-disable-next-line */
-export interface ButtonProps {}
-
-export function Button(props: ButtonProps) {
-  return (
-    <div className={styles['container']}>
-      <h1>Welcome to Button!</h1>
-    </div>
-  );
+export interface ccButton {
+  id?: string;
+  name?: string;
+  onClick?: (e: MouseEvent<HTMLButtonElement>) => void; // handles click events
+  onFocus?: FocusEventHandler<HTMLButtonElement>;
+  type?: 'primary' | 'secondary' | 'error'; // defaults to 'default'
+  size?: 'small' | 'large'; // defaults to 'normal'
+  label?: string;
+  fullWidth?: boolean;
+  variant?: 'text' | 'contained' | 'outlined';
+  state?: 'success' | 'error';
+  loading?: boolean;
+  disabled?: boolean;
+  content?: any;
 }
+
+export const Button: FC<ccButton> = props => {
+  const buttonStyle = classname('button', {
+    'button__full-width': props.fullWidth,
+    'button__loading': props.loading,
+    'button__disabled': props.disabled,
+    'button__small': props.size === 'small',
+    'button__large': props.size === 'large',
+  });
+
+  return (
+    <button
+      id={props.id}
+      className={buttonStyle}
+      name={props.name}
+      disabled={props.disabled}
+      onClick={props.onClick}
+      onFocus={props.onFocus}
+    >
+      {props.content}
+    </button>
+  );
+};
 
 export default Button;
